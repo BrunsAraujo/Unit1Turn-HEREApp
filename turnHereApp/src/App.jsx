3cc0c5d44bd8cfe0b3238de9c86ff2398759cee2
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-11-17 14:17:21 -0600

    Rendering conditionally in single view/static. Using selectedCity state to toggle between CityList and CityItinerary

--- turnHereApp/src/App.jsx
+++ turnHereApp/src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+//rendering conditionally in single view/static. Using selectedCity state to toggle between CityList and CityItinerary
+import CollapsibleMenu from "./components/CollapsibleMenu.jsx";
+import Header from "./components/Header.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [selectedCity, setSelectedCity] = useState(null);
 
   return (
-    <>
-      <div>
-
-
-      </div>
-
-    </>
-  )
+    <div className="app">
+      <Header />
+      <CollapsibleMenu/>
+      
+      {/* Other components go here */}
+   <h1>Weather Forecast</h1>
+      {!selectedCity ? (
+        <CityList onSelectCity={setSelectedCity} />
+      ) : (
+        <cityItinerary city={selectedCity} onBack={() => setSelectedCity(null)} />
+      )}   
+   
+    </div>
+  );
 }
 
-export default App
+export default App;
