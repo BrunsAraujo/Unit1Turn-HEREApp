922583ab26f3a643e88c4bd74ccb3b98f1db29eb
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-11-18 09:31:08 -0600

    Commit to main

--- turnHereApp/src/App.jsx
+++ turnHereApp/src/App.jsx
@@ -1,38 +1,55 @@
-//adds the Search, Footer and routes both. CityItinerary3 supports both props and URL. It betters the user experience.
+//App7.jsx adds the about page and routes it
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import About from "./components/About";
 import CollapsibleMenu from "./components/CollapsibleMenu";
 import CityList from "./components/CityList";
 import CityItinerary from "./components/CityItinerary";
 import Search from "./components/Search.jsx";
 import LoginForm from "./components/LoginForm";
+import TripSaver from "./components/TripSaver";
 import Footer from "./components/Footer.jsx";
 import "./App.css";
 import "./index.css";
 
 function App() {
   const [selectedCity, setSelectedCity] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
   const handleLogin = (username) => {
-    alert(`Welcome, ${username}!`);
+    setLoggedInUser(username);
   };
 
   return (
     <Router>
       <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", backgroundColor: "#f0f8ff" }}>
+        <header>
         <Header />
-        <CollapsibleMenu />
+        </header>
 
-        {/* Main content area */}
-        <div style={{ flex: 1 }}>
+        <nav>
+        <CollapsibleMenu />
+        </nav>
+        <main div style={{ flex: 1 }}>
           <Routes>
-            <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+          <Route path="/about" element={<About />} />
+
+            <Route
+              path="/login"
+              element={
+                !loggedInUser ? (
+                  <LoginForm onLogin={handleLogin} />
+                ) : (
+                  <TripSaver user={loggedInUser} />
+                )
+              }
+            />
             <Route
               path="/cities"
               element={
                 <div className="app">
-                  <h1>Cities to Choose</h1>
+                  
                   {!selectedCity ? (
                     <CityList onSelectCity={setSelectedCity} />
                   ) : (
@@ -50,11 +67,12 @@ function App() {
             />
             <Route path="/cities/:cityName" element={<CityItinerary />} />
           </Routes>
+          </main>
         </div>
-
-        {/* Footer stays at the bottom */}
+      <footer>
         <Footer />
-      </div>
+        </footer>
+      
     </Router>
   );
 }
